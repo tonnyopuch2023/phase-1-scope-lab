d1421033d4cca7d7127d4f57518875140b52ee08
tonnyopuch2023 <tonny.opuch@student.moringaschool.com>
2023-12-18 09:57:05 +0300

    done assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName;
+function upperCaseCustomerName() {
+    if (customerName) {
+      customerName = customerName.toUpperCase();
+    }
+  }
+  function setBestCustomer() {
+    bestCustomer = 'not bob';
+  }
+  function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+  }
+  const leastFavoriteCustomer = 'someone';
+
+function changeLeastFavoriteCustomer() {
+  // Trying to change the value of a constant (This will throw an error)
+  leastFavoriteCustomer = 'someone else'; // This line will cause an error
+}
